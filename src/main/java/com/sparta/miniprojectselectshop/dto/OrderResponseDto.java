793975607debe04db91f31f07713166e264efb09
kimdh32022 <kimdh32022@naver.com>
2026-01-30 16:33:36 +0900

    자리변경

--- src/main/java/com/sparta/miniprojectselectshop/dto/OrderResponseDto.java
+++ src/main/java/com/sparta/miniprojectselectshop/dto/OrderResponseDto.java
@@ -16,15 +16,15 @@ public class OrderResponseDto {
 
     public static  OrderResponseDto from(Order order){
 
-//       return new OrderResponseDto(
-//               order.getOrderId(),
-//               order.getProduct().getProductName(),
-//               order.getProduct().getProductPrice()
-//       ); // 빌더사용 Why?  생성자 사용으로 만드는 객체는 파라미터에 변수 위치가 중요!!
         return OrderResponseDto.builder()
                 .orderId(order.getOrderId())
                 .productName(order.getProduct().getProductName())
                 .productPrice(order.getProduct().getProductPrice())
                 .build();
     }
+    //       return new OrderResponseDto(
+    //               order.getOrderId(),
+    //               order.getProduct().getProductName(),
+    //               order.getProduct().getProductPrice()
+    //       ); // 빌더사용 Why?  생성자 사용으로 만드는 객체는 파라미터에 변수 위치가 중요!!
 }
