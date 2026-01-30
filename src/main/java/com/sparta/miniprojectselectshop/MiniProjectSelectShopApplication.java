538062ade1df656337f5a6e3bc777156027ded06
kimdh32022 <kimdh32022@naver.com>
2026-01-30 17:10:42 +0900

    chore: initialize miniProject-selectShop standalone repository

---
+++ src/main/java/com/sparta/miniprojectselectshop/MiniProjectSelectShopApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.miniprojectselectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectSelectShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectSelectShopApplication.class, args);
+    }
+
+}
